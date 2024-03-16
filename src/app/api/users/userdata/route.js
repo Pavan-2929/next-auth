19b85a6aa05d8480abcf9a7e90c5332512ce4f74
Pavan-2929 <kapdiyapavan3218@gmail.com>
2024-03-16 15:59:57 +0530

    fix

--- src/app/api/users/userdata/route.js
+++ src/app/api/users/userdata/route.js
@@ -7,17 +7,12 @@ connection();
 
 export async function GET(request) {
   try {
-    // Accessing cookies for static generation is not allowed
-    // const userId = await verifyToken(request);
-
-    // Instead, we'll handle token verification in the API route handler
-    const token = request.cookies.get("token")?.value || "";
+    const token = request.cookies.token || "";
     if (!token) {
       return NextResponse.error("Unauthorized", { status: 401 });
     }
 
     const userId = await verifyToken(token);
-
     if (!userId) {
       return NextResponse.error("Unauthorized", { status: 401 });
     }
