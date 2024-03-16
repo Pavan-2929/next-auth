bffabf773f12d82aedbb540c4ea1b5f703608eb5
Pavan-2929 <kapdiyapavan3218@gmail.com>
2024-03-16 15:57:28 +0530

    fix

--- src/app/api/users/userdata/route.js
+++ src/app/api/users/userdata/route.js
@@ -1,37 +1,34 @@
-// pages/api/users/userdata.js
-
 import { verifyToken } from "@/helpers/verifyToken";
+import { NextResponse } from "next/server";
 import User from "@/models/user.model";
 import connection from "@/dbConfig/dbConfig";
-import { NextApiRequest, NextApiResponse } from "next";
 
 connection();
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export async function GET(request) {
   try {
-    const token = req.cookies.token;
+    // Accessing cookies for static generation is not allowed
+    // const userId = await verifyToken(request);
 
+    // Instead, we'll handle token verification in the API route handler
+    const token = request.cookies.get("token")?.value || "";
     if (!token) {
-      return res.status(401).json({ error: "Unauthorized" });
+      return NextResponse.error("Unauthorized", { status: 401 });
     }
 
     const userId = await verifyToken(token);
 
     if (!userId) {
-      return res.status(401).json({ error: "Unauthorized" });
+      return NextResponse.error("Unauthorized", { status: 401 });
     }
 
     const user = await User.findById(userId).select("-password");
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return NextResponse.error("User not found", { status: 404 });
     }
-
-    res.status(200).json(user);
+    return NextResponse.json(user);
   } catch (error) {
     console.error("Error fetching user data:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+    return NextResponse.error("Internal Server Error", { status: 500 });
   }
 }
